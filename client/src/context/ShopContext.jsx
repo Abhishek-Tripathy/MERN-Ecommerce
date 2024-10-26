37d600e68f825177cb08c6bece78ffdc0e5712dd
Abhishek-Tripathy <134616533+Abhishek-Tripathy@users.noreply.github.com>
2024-10-26 07:52:38 +0530

    Final touch ups

--- client/src/context/ShopContext.jsx
+++ client/src/context/ShopContext.jsx
@@ -9,7 +9,7 @@ export const ShopContext = createContext()
 const ShopContextProvider = ({children}) => {
    const currency = '$'
    const deliveryFee = 10
-   const backendUrl = "http://localhost:8000/"
+   const backendUrl = import.meta.env.VITE_BACKEND_URL
    const [search, setSearch] = useState("")
    const [showSearch, setShowSearch] = useState(false)
    const [cartItems, setCartItems] = useState({})
